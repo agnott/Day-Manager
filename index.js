6918328f33424bd99641833eaf9e820be5255a0b
Andrew Gnott <agnott@nd.edu>
2016-08-07 14:10:25 -0400

    Update postgress info.

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ require('dotenv').config();
 
 var express = require('express');
 var app = express();
+var pg = require('pg');
+pg.defaults.ssl = true
 
 app.get('/', function (req, res) {
   res.send('Hello Worldy!');
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    console.log(client);
+    client.query('SELECT * FROM test', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send({results: result.rows}); }
+    });
+  });
+})
 
 app.listen(process.env.PORT || 5000, function () {
   console.log(`Example app listening on port ${process.env.PORT || 5000}!`);
